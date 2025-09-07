07ec7ced8c820c39297ac78024cabeb618111785
Ugandhar Telaganeni <nitya050905@gmail.com>
2025-09-08 00:11:36 +0530

    Add Inheritance.java with Employee and Programmer classes

---
+++ Inheritance.java
@@ -0,0 +1,18 @@
+// Superclass
+class Employee {
+    float salary = 40000;
+}
+
+// Subclass
+class Programmer extends Employee {
+    int bonus = 10000;
+}
+
+// Main class containing main method
+public class Inheritance {
+    public static void main(String[] args) {
+        Programmer p = new Programmer();
+        System.out.println("Programmer salary is: " + p.salary);
+        System.out.println("Bonus of Programmer is: " + p.bonus);
+    }
+}
